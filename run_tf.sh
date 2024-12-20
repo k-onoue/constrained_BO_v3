582eb21ce1a5b9313473efe6a9312987b1573dce
k-onoue <k.onoue0724@gmail.com>
2024-12-20 16:14:49 +0900

    fix

--- run_tf.sh
+++ run_tf.sh
@@ -26,16 +26,16 @@ run_experiment() {
         --tf_method "$tf_method"
         --tf_rank "$tf_rank"
         --tf_lr 0.01
-        --tf_tol 1e-5
-        --tf_reg_lambda 1e-3
+        --tf_tol 1e-6
+        --tf_reg_lambda 0
         --tf_constraint_lambda 1.0
         # Sampler parameters
         --decomp_iter_num 10
         --mask_ratio 0.9
         --n_startup_trials "$n_startup_trials"  # Use parameter
         # Acquisition function parameters
-        --acquisition_function "ucb"
-        --acq_trade_off_param 3.0
+        --acquisition_function "ei"
+        --acq_trade_off_param 1.0
         # Other parameters
         --iter_bo "$iter_bo"  # Use parameter
         --plot_save_dir "$plot_save_dir"
@@ -59,17 +59,17 @@ mkdir -p "$results_dir" "$plot_save_dir"
 cp "$0" "$results_dir"
 
 # Experiment configurations
-map_options=(1 2)
+map_options=(1)
 tf_methods=("cp")
 tf_ranks=(3)
-seed_list=(0)
-constraint=true
+seed_list=(0 1 2 3 4 5 6 7 8 9)
+constraint=false
 direction=false
 decomp_parallel=true
 tf_max_iter=1000
 
-n_startup_trials=10  # Added
-iter_bo=12          # Added
+n_startup_trials=1  # Added
+iter_bo=2000        # Added
 
 # Run experiments
 for map_option in "${map_options[@]}"; do
